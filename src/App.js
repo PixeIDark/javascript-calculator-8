d58e7fbe4dc1d6198390b63f93391f92bf33f780
PixeIDark <kiki7951@naver.com>
2025-10-19 01:45:17 +0900

    refactor: App.js - 요구 사항 해석 변경 => 로직도 변경
    - 소수점 O => X, why? 커스텀 구분자가 "." 일경우 판정이 애매함
    - "5, ,0" 과 같이 "" 공백은 0으로 해석. 요구사항에서 input이 "" 일경우 0반환이라 명시되어 있어서 일관성 맞춤

--- src/App.js
+++ src/App.js
@@ -7,40 +7,37 @@
       let input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
       if(typeof input !== 'string') throw new Error("[ERROR]")
 
-      if(input === "") {
-        MissionUtils.Console.print(`결과 : 0`)
-        return
-      }
-
       // 2. 커스텀 구분자가 있는지(단일 문자만 가능. 아닐 시 예외 처리) 확인. 있으면 변수에 할당 없으면 기본 구분자 할당
-      let delimiter = /[,;]/
+      const validChars = new Set(["0","1","2","3","4","5","6","7","8","9",""])
+      let delimiter = /[,:]/
 
       if(input.includes("//")) {
-        const customDelimiterMatch = input.match(/^\/\/(.)\\n/)
+        const customDelimiterMatch = input.match(/^\/\/(.)(?:\\n|\r?\n)/)
         if(!customDelimiterMatch) throw new Error("[ERROR]")
 
+        // delimiter 타입이 다름
         delimiter = customDelimiterMatch[1]
         input = input.substring(customDelimiterMatch[0].length)
+        validChars.add(delimiter)
+      } else {
+        // 이부분 컹스한데 이게 최선인지??
+        validChars.add(",")
+        validChars.add(":")
       }
 
-      // 3. 입력 구분자와 숫자를 제외한 나머지가 들어간 경우 예외 처리. 단, "."은 소수점으로 쓰인 경우 용인함(양수로 구성된 문자열 조건)
+      // 3. 입력 구분자와 숫자를 제외한 나머지가 들어간 경우 예외 처리. 단, "."은 양수라 조건은 충족하는데 불가능하게 하는게 맞다 예시에 없잖아
+      // 전체 input이 ""인경우는 0 반환하라고 명시, 그렇다면 "5, " 인경우 빈 공백에서 에러 트로우할지 0 반환할지
+      const isInvalid = input.split("").some(char => !validChars.has(char))
+      // isInvalid 하나로 음수가 아닌지, 소수인지 다 파악하고있는데 맞는지 이방식이
+      // 예외에 따른 명확한 에러 메세지 바운딩을 해줄꺼면 이 방식은 틀렸음. 반면 모두 같게할꺼면 이게 간단함
+      if(isInvalid) throw new Error("[ERROR]")
+
       input = input.split(delimiter)
       console.log({input})
+      const result = input.reduce((acc, str) => acc + Number(str), 0)
 
-      let sum = 0
-      // input 의 각 요소가 Number(input) 시, isNAN 이면 예외 처리해야함
-      for(const str of input) {
-        if(str === "") throw new Error("[ERROR]")
-
-        const isValidNumber = /^\d+(\.\d+)?$/.test(str)
-        if(!isValidNumber) throw new Error("[ERROR]")
-
-        const num = Number(str)
-        sum += num
-      }
-
-      // 4. 3번 과정에서 동시에 누적합도 진행. 소수점 .5 || 5. 예외처리
-      MissionUtils.Console.print(`결과 : ${sum}`)
+      // 4. 3번 과정에서 동시에 누적합도 진행.
+      MissionUtils.Console.print(`결과 : ${result}`)
     }
   }
 
