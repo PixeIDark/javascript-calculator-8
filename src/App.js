5693427ec39a5ca7288251941dfd8ad5abbab862
PixeIDark <kiki7951@naver.com>
2025-10-19 10:48:40 +0900

    refactor: App.js - 인풋과 아웃풋만 관리하고, 계산은 클래스 메서드 체이닝
    - 추상화 레벨을 어느정도로 해야할지 가늠이 안됨
    - 각 과정을 같은 계층에 둠으로써 보기가 편함 함수형으로 하면 사이드 이펙트 조절 분리가 쉽지만 가독성은 구릴듯

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-  import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
+import {StringCalculator} from "./StringCalculator.js";
 
-  // 커스텀 구분자 존재하면 그것을 기준으로 삼고, 없으면 기본 구분자를 기준으로한다
-  class App {
-    async run() {
-      // 1. 입력 string 아닌 경우 예외 처리
-      let input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
-      if(typeof input !== 'string') throw new Error("[ERROR]")
+class App {
+  async run() {
+    const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
 
-      // 2. 커스텀 구분자가 있는지(단일 문자만 가능. 아닐 시 예외 처리) 확인. 있으면 변수에 할당 없으면 기본 구분자 할당
-      const validChars = new Set(["0","1","2","3","4","5","6","7","8","9"," "])
-      let delimiter = /[,:]/
+    if(typeof input !== 'string') throw new Error("[ERROR]")
 
-      if(input.includes("//")) {
-        const customDelimiterMatch = input.match(/^\/\/(.)(?:\\n|\r?\n)/)
-        if(!customDelimiterMatch) throw new Error("[ERROR]")
+    const result = new StringCalculator(input).parseDelimiter().validateInput().calculate()
 
-        // delimiter 타입이 다름
-        delimiter = customDelimiterMatch[1]
-        input = input.substring(customDelimiterMatch[0].length)
-        validChars.add(delimiter)
-      } else validChars.add(",").add(":")
-
-
-      // 3. 입력 구분자와 숫자를 제외한 나머지가 들어간 경우 예외 처리. 단, "."은 양수라 조건은 충족하는데 불가능하게 하는게 맞다 예시에 없잖아
-      // 전체 input이 ""인경우는 0 반환하라고 명시, 그렇다면 "5, " 인경우 빈 공백에서 에러 트로우할지 0 반환할지
-      const isInvalid = input.split("").some(char => !validChars.has(char))
-      // isInvalid 하나로 음수가 아닌지, 소수인지 다 파악하고있는데 맞는지 이방식이
-      // 예외에 따른 명확한 에러 메세지 바운딩을 해줄꺼면 이 방식은 틀렸음. 반면 모두 같게할꺼면 이게 간단함
-      // 테스트 코드에  await expect(app.run()).rejects.toThrow("[ERROR]"); 명시되어있어서 에러메시지 "[ERROR]") 고정. 테케 실패하면 불합격 간주한다함(테스트가 실패하면 점수가 0점이 되므로 제출하기 전에 반드시 확인한다.).
-      if(isInvalid) throw new Error("[ERROR]")
-
-      input = input.split(delimiter)
-      console.log({input})
-      const result = input.reduce((acc, str) => acc + Number(str), 0)
-
-      // 4. 3번 과정에서 동시에 누적합도 진행.
-      MissionUtils.Console.print(`결과 : ${result}`)
-    }
+    MissionUtils.Console.print(`결과 : ${result}`)
   }
+}
 
-  export default App;
+export default App;
